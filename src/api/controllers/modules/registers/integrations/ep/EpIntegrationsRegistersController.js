ef1e6560f327073db4b58e8007fa00c18128419a
unknown <aalencarvz@gmail.com>
2024-11-19 15:34:58 -0300

    adjusts

--- src/api/controllers/modules/registers/integrations/ep/EpIntegrationsRegistersController.js
+++ src/api/controllers/modules/registers/integrations/ep/EpIntegrationsRegistersController.js
@@ -190,15 +190,16 @@ class EpIntegrationsRegistersController extends RegistersController{
      *************************************************/ 
     
 
-    static async getRcasCodes(req) {
+    static async getRcasCodes(params) {
         let rcas = null;
-        if (req?.user.access_profile_id == Access_Profiles.SUPERVISOR) {
+        console.log('xxxxnnnn',params);
+        if (params?.user.access_profile_id == Access_Profiles.SUPERVISOR) {
             let dataRel = await Relationships.getModel().findAll({
                 raw:true,
                 where:{
                     relationship_type_id: [Relationship_Types.EP_ID],
                     table_1_id: Users.id,
-                    record_1_id: req.user.id,
+                    record_1_id: params.user.id,
                     table_2_id: EpTrabalhadores.id
                 }
             });
@@ -233,7 +234,7 @@ class EpIntegrationsRegistersController extends RegistersController{
                 where:{
                     relationship_type_id: [Relationship_Types.EP_ID],
                     table_1_id: Users.id,
-                    record_1_id: req?.user.id ,
+                    record_1_id: params?.user.id ,
                     table_2_id: EpVendedores.id
                 }
             });
@@ -241,14 +242,14 @@ class EpIntegrationsRegistersController extends RegistersController{
                 rcas = dataRel.map(el=>el.record_2_id).join(',');
             }
         }
-        if (Utils.hasValue(req.body.seller_ids)) {
+        if (Utils.hasValue(params.seller_ids)) {
             if (Utils.hasValue(rcas)) {                
-                let seller_ids = Utils.toArray(req.body.seller_ids);
+                let seller_ids = Utils.toArray(params.seller_ids);
                 let rcasTemp = Utils.toArray(rcas);
                 seller_ids = seller_ids.filter(el=>rcasTemp.indexOf(el) > -1);
                 rcas = seller_ids.join(',');                
             } else {
-                rcas = Utils.toArray(req.body.seller_ids).join(',');
+                rcas = Utils.toArray(params.seller_ids).join(',');
             }
         }
         if (!rcas) rcas = `select ev.cod from EP.EPVENDEDORES ev`;
