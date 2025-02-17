75cc52b3bdbd465254f8ac0e001675cb09933c9e
unknown <aalencarvz@gmail.com>
2025-02-17 18:47:55 -0300

    implemented view items of inputs

--- src/api/controllers/modules/integrations/sjd/registers/SjdLogAnaliseEntController.ts
+++ src/api/controllers/modules/integrations/sjd/registers/SjdLogAnaliseEntController.ts
@@ -4,6 +4,7 @@ import SjdLogAnaliseEnt from "../../../../../database/models/sjd/SjdLogAnaliseEn
 import DataSwap from "../../../../data/DataSwap.js";
 import Utils from "../../../../utils/Utils.js";
 import SjdBaseRegistersIntegrationsController from "./SjdBaseRegistersIntegrationsController.js";
+import Report_Data_Founts from "../../../../../database/models/Report_Data_Founts.js";
 
 export default class SjdLogAnaliseEntController extends SjdBaseRegistersIntegrationsController{
     static getTableClassModel() : any {
@@ -13,62 +14,107 @@ export default class SjdLogAnaliseEntController extends SjdBaseRegistersIntegrat
     static async getInputAnalise(params?: any) : Promise<DataSwap> {
         let result : DataSwap = new DataSwap();
         try {   
-            let where : any = [];
+            let reportDataFountName = 'input analises';
 
+            let query : any = await Report_Data_Founts.findOne({
+                where:{
+                    name: reportDataFountName
+                }
+            });
 
-            if (Utils.hasValue(params.business_units)) {
-                where.push(`e.codfilial in (${params.business_units})`)
-            }
-            if (Utils.hasValue(params.input_transcation_number)) {
-                where.push(`e.numtransent in (${params.input_transcation_number})`)
-            }
-            if (Utils.hasValue(params.invoice_number)) {
-                where.push(`e.numnota in (${params.invoice_number})`)
-            }  
-            if (Utils.hasValue(params.start_date)) {
-                where.push(`e.dtent >= to_date('${params.start_date}','yyyy-mm-dd')`)
-            } 
-            if (Utils.hasValue(params.end_date)) {
-                where.push(`e.dtent <= to_date('${params.end_date}','yyyy-mm-dd')`)
-            } 
-            if (where.length) {
-                where = ` where ${where.join(' and ')}`;
-            } else {
-                where = ` where 1 = 2`;
-            }
+            if (Utils.hasValue(query)) {
+                query = query.get_value_from;
 
-            let query = `
-                select 
-                    case when e.numped is not null then 'PREENT' ELSE 'ENTRADA' end as "tipo",
-                    e.codfilial as "codfilial",
-                    e.numtransent as "numtransent",
-                    e.numnota as "numnota",
-                    e.numped as "numped",
-                    e.dtent as "dtent",
-                    e.rotinalanc as "rotinalanc",
-                    e.equiplanc as "equiplanc",
-                    max(e.temdivergenciatrib) as "temdivergenciatrib",
-                    max(e.temdivergenciapreco) as "temdivergenciapreco",
-                    max(e.temdivergenciaprecotabfornec) as "temdivergenciaprecotabfornec"
-                from
-                    sjdloganaliseent e
-                ${where}
-                group by
-                    case when e.numped is not null then 'PREENT' ELSE 'ENTRADA' end,
-                    e.codfilial,
-                    e.numtransent,
-                    e.numnota,
-                    e.numped,
-                    e.dtent,
-                    e.rotinalanc,
-                    e.equiplanc
-            `
-            result.data = await DBConnectionManager.getConsultDBConnection()?.query(
-                query,{
+                let where : any = [];
+
+                if (Utils.hasValue(params.business_units)) {
+                    where.push(`e.codfilial in (${params.business_units})`)
+                }
+                if (Utils.hasValue(params.input_transcation_number)) {
+                    where.push(`e.numtransent in (${params.input_transcation_number})`)
+                }
+                if (Utils.hasValue(params.invoice_number)) {
+                    where.push(`e.numnota in (${params.invoice_number})`)
+                }  
+                if (Utils.hasValue(params.start_date)) {
+                    where.push(`trunc(e.dtent) >= trunc(to_date('${params.start_date}','yyyy-mm-dd'))`)
+                } 
+                if (Utils.hasValue(params.end_date)) {
+                    where.push(`trunc(e.dtent) <= trunc(to_date('${params.end_date}','yyyy-mm-dd'))`)
+                } 
+                if (where.length) {
+                    where = ` where ${where.join(' and ')}`;
+                } else {
+                    where = ` where 1 = 2`;
+                }
+
+                query = query.replace('${where}',where);
+                
+                result.data = await DBConnectionManager.getConsultDBConnection()?.query(query,{
                     type: QueryTypes.SELECT
-                }                
-            )
-            result.success = true;
+                })
+                result.success = true;
+            } else {
+                throw new Error(`report data fount not found with name ${reportDataFountName}`);
+            }
+        } catch (e: any) {
+            result.setException(e);
+        }
+        return result;
+    }
+
+    static async getInputAnaliseDetails(params?: any) : Promise<DataSwap> {
+        let result : DataSwap = new DataSwap();
+        try {   
+
+            let reportDataFountName = 'input analise details';
+
+            let query : any = await Report_Data_Founts.findOne({
+                where:{
+                    name: reportDataFountName
+                }
+            });
+
+            if (Utils.hasValue(query)) {
+                query = query.get_value_from;
+                let where : any = [];
+
+
+                if (Utils.hasValue(params.business_units)) {
+                    where.push(`e.codfilial in (${params.business_units})`)
+                }
+                if (Utils.hasValue(params.input_transcation_number)) {
+                    where.push(`e.numtransent in (${params.input_transcation_number})`)
+                }
+                if (Utils.hasValue(params.invoice_number)) {
+                    where.push(`e.numnota in (${params.invoice_number})`)
+                }  
+                if (Utils.hasValue(params.order_number)) {
+                    where.push(`e.numped in (${params.order_number})`)
+                }  
+                if (Utils.hasValue(params.start_date)) {
+                    where.push(`trunc(e.dtent) >= trunc(to_date('${params.start_date.substring(0,19).replace("T"," ")}','yyyy-mm-dd hh24:mi:ss'))`)
+                } 
+                if (Utils.hasValue(params.end_date)) {
+                    where.push(`trunc(e.dtent) <= trunc(to_date('${params.end_date.substring(0,19).replace("T"," ")}','yyyy-mm-dd hh24:mi:ss'))`)
+                } 
+                if (where.length) {
+                    where = ` where ${where.join(' and ')}`;
+                } else {
+                    where = ` where 1 = 2`;
+                }
+
+                query = query.replace('${where}',where);
+                
+                result.data = await DBConnectionManager.getConsultDBConnection()?.query(
+                    query,{
+                        type: QueryTypes.SELECT
+                    }                
+                )
+                result.success = true;
+            } else {
+                throw new Error(`report data fount not found with name ${reportDataFountName}`);
+            }
         } catch (e: any) {
             result.setException(e);
         }
